0778583b9da5fd7e9d8ac9035f0db192590fe95c
Saad Imran <saad.imran@contour-software.com>
2024-01-22 20:30:59 +0500

    C#: code fix

---
+++ module 2 (C#)/module2/CalculatorGeneric.cs
@@ -0,0 +1,35 @@
+namespace module2
+{
+    public class CalculatorGeneric<T> : ICalculator<T>
+    {
+        public T Add(T a, T b)
+        {
+            if (a == null || b == null)
+            {
+                throw new Exception("Arguments Cant be Null");
+            }
+
+            dynamic dynamicA = a;
+            dynamic dynamicB = b;
+
+            return dynamicA + dynamicB;
+        }
+
+        public T Subtract(T a, T b)
+        {
+            if (a == null || b == null)
+            {
+                throw new Exception("Arguments Cant be Null");
+            }
+
+            if(typeof(T) != typeof(int) && typeof(T) != typeof(float) && typeof(T) != typeof(decimal) && typeof(T) != typeof(long) && typeof(T) != typeof(double)){
+                throw new Exception("Subtract can not work for this data type");
+            }
+
+            dynamic dynamicA = a;
+            dynamic dynamicB = b;
+
+            return dynamicA - dynamicB;
+        }
+    }
+}
